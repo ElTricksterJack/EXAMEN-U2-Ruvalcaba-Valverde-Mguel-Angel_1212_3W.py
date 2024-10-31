82976cbf879592684cc0d64d5e8448a4e33b0981
ElTricksterJack <a.23308051281212@cbtis128.edu.mx>
2024-10-31 15:39:38 -0600

    Update punto 2.py

--- punto 2.py
+++ punto 2.py
@@ -1,15 +1,15 @@
 print("\nRuvalcaba Valverde Mguel Angel_1212_3W")
 print("--------Punto 2--------")
-cur = {
+cur = {#primero hay que haser el dicionario, 
     "Esgrima": 0,
     "Matematicas": 0,
     "Biologia": 0,
     "Estudio de Idiomas Antiguas": 0,
     "Escritura": 0
 }
-print("--Allade los creditos que tiene--")
+print("--Allade los creditos que tiene--")#luego hay que actualisar los valores de cada curso
 C1 = float(input(f"Calisficasion de Esgrima:"))
-cur["Esgrima"] = C1
+cur["Esgrima"] = C1#usndo este codigo *Cara de Okarun mostrando un poster de aliens*
 C2 = float(input(f"Calisficasion de Matematicas:"))
 cur["Matematicas"] = C2
 C3 = float(input(f"Calisficasion de Biologia:"))
@@ -18,13 +18,14 @@ C4 = float(input(f"Calisficasion de Estudio de Idiomas Antiguas:"))
 cur["Estudio de Idiomas Antiguas"] = C4
 C5 = float(input(f"Calisficasion de Escritura:"))
 cur["Escritura"] = C5
-print("--Mostrar Creditos Optenidos--")
-print(f"Los creditos son {cur["Esgrima"]}, en Esgrima.") 
-print(f"Los creditos son {cur["Matematicas"]}, en Matematicas.") 
+print("--Mostrar Creditos Optenidos--")#despues qui veras cuantos creditos tiene en cada curso y en cual
+print(f"Los creditos son {cur["Esgrima"]}, en Esgrima.") #dos en uno
+print(f"Los creditos son {cur["Matematicas"]}, en Matematicas.") #bueno esas fueron las espesificasiones.
 print(f"Los creditos son {cur["Biologia"]}, en Biologia.") 
 print(f"Los creditos son {cur["Estudio de Idiomas Antiguas"]}, en Estudio de Idiomas Antiguas.") 
 print(f"Los creditos son {cur["Escritura"]}, en Escritura.") 
-
+#OMG, Ho yes, La primavera es lo Mejor
+#perond queria haser una referensia a undead unluck
 print("--------------------------------------")
 print("Objetivo: Escribir un programa que almacene el diccionario\ncon los créditos de las asignaturas de un curso {'Matemáticas': 6, 'Física': 4, 'Química': 5} y\ndespués muestre por pantalla los créditos de cada asignatura en el\nformato <asignatura>\ntiene <créditos> créditos, donde <asignatura> es\ncada una de las asignaturas del curso, y <créditos> son sus créditos.\nAl final debemostrar también el número total de créditos del curso.\n")
 print("Resultado: se bieron los creditos de cada respectivo curso y el nombre del curso.\n")
