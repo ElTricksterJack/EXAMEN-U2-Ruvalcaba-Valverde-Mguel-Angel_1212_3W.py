bc739c6318757cc6fb8ea02dcb0d7975b48424aa
ElTricksterJack <a.23308051281212@cbtis128.edu.mx>
2024-10-31 15:48:10 -0600

    Create punto 3.py
    Escribir un programa que almacene las
    asignaturas de un curso (por ejemplo Matemáticas, Física, Química, Historia y
    Lengua) en una lista, pregunte al usuario la nota que ha sacado en cada
    asignatura, y después las muestre por pantalla con el mensaje En <asignatura> has sacado <nota> donde <asignatura> es
    cada una des las asignaturas de la lista y <nota> cada una de las correspondientes

---
+++ punto 3.py
@@ -0,0 +1,26 @@
+print("\nRuvalcaba Valverde Mguel Angel_1212_3W")
+print("--------Punto 3--------")
+print("-- Oh el olvidado en nombre de tus materias *dise el maestro*\nPodrias recordarlas porfavor")#oscura referensia a pokemon
+M1 = input("Materia 1: ")#nombrar cada una de las calificasiones
+M2 = input("Materia 2: ")
+M3 = input("Materia 3: ")
+M4 = input("Materia 4: ")
+M5 = input("Materia 5: ")
+M = (M1,M2,M3,M4,M5)#meter todas las calificasiones en una lista tal y como se pidio
+print("-- Oh el olvidado en las calificasiones *dise el maestro otraves*\nPodrias recordarlas porfavor")
+C1 = int(input(f"Calisficasion de {M[0]}:"))#darle una valor a su calificasion
+C2 = int(input(f"Calisficasion de {M[1]}:"))
+C3 = int(input(f"Calisficasion de {M[2]}:"))
+C4 = int(input(f"Calisficasion de {M[3]}:"))
+C5 = int(input(f"Calisficasion de {M[4]}:"))
+C = [C1,C2,C3,C4,C5]#meterlas a una lista diferente
+print("-- Aqui tienes, querio alubmno")
+print(f"La calificasion de {M[0]} es {C[0]}")#ahora mostrarlas calificasiones de cada materia
+print(f"La calificasion de {M[1]} es {C[1]}")
+print(f"La calificasion de {M[2]} es {C[2]}")
+print(f"La calificasion de {M[3]} es {C[3]}")
+print(f"La calificasion de {M[4]} es {C[4]}")
+#sabia que era bueno tener plantillas propias con migo
+print("--------------------------------------")
+print("Objetivo: Escribir un programa que almacene las\nasignaturas de un curso (por ejemplo Matemáticas, Física, Química, Historia y Lengua) en \nuna lista, pregunte al usuario la nota que ha sacado en cada\nasignatura, y después las muestre por pantalla con el mensaje En <asignatura> has sacado <nota> donde <asignatura> es\ncada una des las asignaturas de la lista y <nota> cada una de las correspondientes\n")
+print("Resultado: tremendo uso de aunto plantillas, pero sengun las instrusiones del profe hasi esta bien, creo yo.\n")
